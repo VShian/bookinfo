857b079112f5c6cc865337d8d43f0b62bb2f5c7d
Vishnu <vvardhanreddy4@gmail.com>
2019-02-17 12:21:13 +0530

    made code to work without opencv
    removed opencv usage and used PIL.Image library

--- webpage/views.py
+++ webpage/views.py
@@ -10,18 +10,18 @@ from PIL import Image
 
 def ocr(img_path):
 	# Recognize text with tesseract for python
-	img = cv2.imread(os.path.abspath('.')+img_path, 0)
-	img = cv2.resize(img, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
+# 	img = cv2.imread(os.path.abspath('.')+img_path, 0)
+# 	img = cv2.resize(img, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
 	# Apply dilation and erosion to remove some noise
-	kernel = np.ones((1, 1), np.uint8)
-	img = cv2.dilate(img, kernel, iterations=1)
-	img = cv2.erode(img, kernel, iterations=1)
+# 	kernel = np.ones((1, 1), np.uint8)
+# 	img = cv2.dilate(img, kernel, iterations=1)
+# 	img = cv2.erode(img, kernel, iterations=1)
 	# Apply blur to smooth out the edges
-	img = cv2.GaussianBlur(img, (5, 5), 0)
+# 	img = cv2.GaussianBlur(img, (5, 5), 0)
 	# Apply threshold to get image with only b&w (binarization)
-	img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-	
-	result = image_to_string(img)
+# 	img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+	path = os.path.abspath('.')+img_path
+	result = image_to_string(Image.open(path))
 	result = result.split('\n')
 
 	# print(result)
